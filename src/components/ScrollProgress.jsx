75cb80aa3f12b1fa4315c8d1baeb2534a39e885e
yaswanthsingampalli <yaswanthkumarsingampalli77@gmail.com>
2026-07-22 13:37:34 -0500

    Add ScrollProgress component for scroll tracking

---
+++ src/components/ScrollProgress.jsx
@@ -0,0 +1,21 @@
+// src/components/ScrollProgress.jsx
+import React from 'react';
+import { motion, useScroll, useSpring } from 'framer-motion';
+
+const ScrollProgress = () => {
+  const { scrollYProgress } = useScroll();
+  const scaleX = useSpring(scrollYProgress, {
+    stiffness: 200,
+    damping: 30,
+    restDelta: 0.001,
+  });
+
+  return (
+    <motion.div
+      className="fixed top-0 left-0 right-0 h-1 bg-primary origin-left z-[60]"
+      style={{ scaleX }}
+    />
+  );
+};
+
+export default ScrollProgress;
